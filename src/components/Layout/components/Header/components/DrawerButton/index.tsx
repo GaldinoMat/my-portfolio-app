91d34656ac018a0ef5060865f3ad37c8a0875855
GaldinoMat <mateusgaldino1000@hotmail.com>
2023-03-18 15:48:36 -0300

    feat: :sparkles: implement locale changer

--- src/components/Layout/components/Header/components/DrawerButton/index.tsx
+++ src/components/Layout/components/Header/components/DrawerButton/index.tsx
@@ -5,7 +5,7 @@ function DrawerButton({ isNavOpen, setIsNavOpen }: DrawerType) {
     <section className="flex justify-end items-center">
       <button
         type="button"
-        className="w-12 h-8 flex flex-col justify-between items-end p-[.375rem] z-30 group"
+        className="w-8 h-8 flex flex-col justify-between items-end py-[.375rem] z-30 group"
         onClick={() => setIsNavOpen(!isNavOpen)}
       >
         <span
@@ -19,8 +19,8 @@ function DrawerButton({ isNavOpen, setIsNavOpen }: DrawerType) {
           }`}
         />
         <span
-          className={`block w-4/5 h-[3px] bg-[#A6A6A6] dark:bg-slate-300 transition-all group-hover:w-full group-hover:bg-[#292929] dark:group-hover:bg-[#A6A6A6] ${
-            isNavOpen ? "w-full -rotate-45 -translate-y-[0.6rem]" : ""
+          className={`block w-full h-[3px] bg-[#A6A6A6] dark:bg-slate-300 transition-all group-hover:w-full group-hover:bg-[#292929] dark:group-hover:bg-[#A6A6A6] ${
+            isNavOpen ? "-rotate-45 -translate-y-[0.6rem]" : ""
           }`}
         />
       </button>
--- src/components/Layout/components/Header/index.tsx
+++ src/components/Layout/components/Header/index.tsx
@@ -1,3 +1,6 @@
+import { useLocale } from "@/hooks/useLocale"
+import Link from "next/link"
+import { useRouter } from "next/router"
 import { useState } from "react"
 import DrawerButton from "./components/DrawerButton"
 import DrawerMenu from "./components/DrawerMenu"
@@ -6,9 +9,16 @@ import ThemeChanger from "./components/ThemeChanger"
 function Header() {
   const [isNavOpen, setIsNavOpen] = useState(false)
 
+  const { secondLocale } = useLocale()
+
+  const { asPath } = useRouter()
+
   return (
     <header className="w-full bg-transparent fixed top-0 z-20">
-      <section className="py-4 max-w-xs lg:max-w-5xl mx-auto flex justify-end">
+      <section className="py-4 max-w-xs lg:max-w-5xl mx-auto gap-3 flex justify-end items-center">
+        <Link href={asPath} locale={secondLocale}>
+          {secondLocale?.toUpperCase()}
+        </Link>
         <ThemeChanger />
         <DrawerButton isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
         <DrawerMenu isNavOpen={isNavOpen} setIsNavOpen={setIsNavOpen} />
