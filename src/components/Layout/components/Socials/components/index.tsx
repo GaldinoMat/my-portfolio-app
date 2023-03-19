a4f21cac341e45773d670d07be369ff6127f46b9
GaldinoMat <mateusgaldino1000@hotmail.com>
2023-03-19 17:45:53 -0300

    refactor: minor eslint fix

--- src/components/Layout/components/Socials/components/index.tsx
+++ src/components/Layout/components/Socials/components/index.tsx
@@ -3,7 +3,7 @@ import { InfoType } from "./typings/types"
 
 function SocialLink({ logoSource, alt, href }: InfoType) {
   return (
-    <div className="w-6 h-6">
+    <div className="h-6 w-6">
       <a href={href}>
         <Image src={logoSource} alt={alt} />
       </a>
