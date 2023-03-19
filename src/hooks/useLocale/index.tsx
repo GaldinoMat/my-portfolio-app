95817fa599a68240c2ec6e5626ba4a1f195a388e
GaldinoMat <mateusgaldino1000@hotmail.com>
2023-03-19 17:26:31 -0300

    perf: :zap: implement cache for translation code

--- src/hooks/useLocale/index.tsx
+++ src/hooks/useLocale/index.tsx
@@ -6,6 +6,7 @@ import {
   useContext,
   useEffect,
   useMemo,
+  useRef,
   useState,
 } from "react"
 
@@ -34,22 +35,33 @@ export function LocaleProvider({ children }: LocaleProviderType) {
   const [secondLocale, setSecondLocale] = useState(
     locales?.find((cur) => cur !== locale) as string
   )
-  const [messages, setMessages] = useState<LocaleMessages>({})
+  const messagesCache = useRef<Record<string, LocaleMessages>>({})
+  const [messages, setMessages] = useState<LocaleMessages>(() => {
+    if (locale && messagesCache.current[locale]) {
+      return messagesCache.current[locale]
+    }
+    return {}
+  })
 
   const handleLocaleChange = useCallback(async () => {
     setSecondLocale(locales?.find((cur) => cur !== locale) as string)
 
-    try {
-      const data = await fetch(
-        `${window.location.origin}/locales/${locale}.json`
-      ).then((resp) => {
-        return resp.json()
-      })
-      setMessages(data)
-    } catch (error) {
-      // eslint-disable-next-line no-console
-      console.error(`Error loading messages for locale ${locale}: ${error}`)
-      setMessages({})
+    if (locale && messagesCache.current[locale]) {
+      setMessages(messagesCache.current[locale])
+    } else {
+      try {
+        const data = await fetch(
+          `${window.location.origin}/locales/${locale}.json`
+        ).then((resp) => {
+          return resp.json()
+        })
+        messagesCache.current[locale as string] = data
+        setMessages(data)
+      } catch (error) {
+        // eslint-disable-next-line no-console
+        console.error(`Error loading messages for locale ${locale}: ${error}`)
+        setMessages({})
+      }
     }
   }, [locale, locales])
 
